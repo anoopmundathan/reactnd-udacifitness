cb7fdba5ebe52772ce1f5bd01d53883a7d247e5d
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-10-21 15:38:40 +0100

    Create History component

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
+
+const logger = createLogger()
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={createStore(reducer, applyMiddleware(logger))}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,7 @@
 export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES'
 export const ADD_ENTRY = 'ADD_ENTRY'
 
-export function receiveEntry(entries) {
+export function receiveEntries(entries) {
   return {
     type: RECEIVE_ENTRIES,
     entries
