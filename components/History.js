cef3402024bf994a63034272a1fc8d32a3c1613c
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-10-21 21:07:39 +0100

    Add Loading

--- components/History.js
+++ components/History.js
@@ -8,8 +8,13 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import DateHeader from './DateHeader'
 import { white } from '../utils/colors'
 import MetricCard from './MetricCard'
+import { AppLoading } from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false
+  }
+
   componentDidMount() {
     const { dispatch } = this.props
     fetchCalendarResults()
@@ -21,6 +26,9 @@ class History extends Component {
           }))
         }
       })
+      .then(() => this.setState(() => ({
+        ready: true
+      })))
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -51,6 +59,11 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if(ready === false) {
+      return <AppLoading />
+    }
     return(  
       <UdaciFitnessCalendar 
         items={entries}
