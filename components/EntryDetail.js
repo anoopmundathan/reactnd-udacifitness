be907a9a227f256917cbdd5497ae952b606a14ee
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-10-23 20:16:21 +0100

    Show metricCard

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
 class EntryDetail extends Component {
   render() {
+    const { metrics } = this.props
     return(
       <View>
+        <MetricCard metrics={metrics} />
         <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
       </View>
     )
   }
 }
 
-export default EntryDetail
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params
+  return {
+    entryId,
+    metrics: state[entryId]
+  }
+}
+
+export default connect(mapStateToProps)(EntryDetail)
