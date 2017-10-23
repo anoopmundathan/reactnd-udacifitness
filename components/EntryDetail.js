8cd3cfc9c9ed9df710ad51ba8909d31afd8ea187
Anoop Mundathan <anoop.mundathan@gmail.com>
2017-10-23 21:26:38 +0100

    Display date in the header

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -3,17 +3,36 @@ import { View, Text, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { white } from '../utils/colors'
 import MetricCard from './MetricCard'
+
 class EntryDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params
+    const year = entryId.slice(0, 4)
+    const month = entryId.slice(5, 7)
+    const day = entryId.slice(8)
+
+    return {
+      title: `${month}/${day}/${year}`
+    }
+  }
+
   render() {
     const { metrics } = this.props
     return(
-      <View>
+      <View style={styles.container}>
         <MetricCard metrics={metrics} />
-        <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15
+  }
+})
 
 function mapStateToProps(state, { navigation }) {
   const { entryId } = navigation.state.params
